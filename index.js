257f54b8a17b0169506969f007329240bd24acd4
akash <hafjmohammadakash@gmail.com>
2025-12-25 00:57:17 -0800

    text animation done with logo

--- index.js
+++ index.js
@@ -49,3 +49,24 @@ gsap.from("#header-text .a", {
   stagger: 0.1,
   delay: 1,
 });
+// header texr animation
+function breackTheText1() {
+  var hi = document.querySelector("#header-text1");
+  var h1Text = hi.textContent;
+  var splitText = h1Text.split("");
+  let clutte = "";
+
+  splitText.forEach(function (char) {
+    clutte += `<span class="a">${char}</span>`;
+  });
+  hi.innerHTML = clutte;
+}
+breackTheText1();
+
+gsap.from("#header-text1 .a", {
+  opacity: 0,
+  y: 80,
+  duration: 0.5,
+  stagger: 0.1,
+  delay: 1,
+});
