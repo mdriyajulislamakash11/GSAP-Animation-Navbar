607b4287fe605ba04d40e5b816e67696836fb6cf
akash <hafjmohammadakash@gmail.com>
2025-12-24 22:47:58 -0800

    navbar done

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 
+var menu = document.querySelector("#nav i");
+var close = document.querySelector("#full i");
 
 var tl = gsap.timeline();
 
 tl.to("#full", {
     right: 0,
-    duration: 1,
+    duration: 0.8,
 })
 
 tl.from("#full h4", {
     opacity: 0,
     x: 50,
-    duration: 0.8,
+    duration: 0.5,
     stagger: 0.2,
 })
 
 tl.from("#full i", {
     opacity: 0,
+})
+
+tl.pause();
+
+menu.addEventListener("click", function() {
+    tl.play();
+})
+close.addEventListener("click", function() {
+    tl.reverse();
 })
